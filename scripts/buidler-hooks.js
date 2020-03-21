9e778b58e21eada6a3e05322f0e0f6fefbb02b25
Gabriel Garcia <gabrielpk.18@gmail.com>
2020-03-21 01:16:04 -0300

    Add init value

--- scripts/buidler-hooks.js
+++ scripts/buidler-hooks.js
@@ -27,7 +27,7 @@ module.exports = {
   // Called when the start task needs to know the app proxy's init parameters.
   // Must return an array with the proxy's init parameters.
   getInitParams: async ({}, { web3, artifacts }) => {
-    return [];
+    return [15];
   },
 
   // Called after the app's proxy is updated with a new implementation.
